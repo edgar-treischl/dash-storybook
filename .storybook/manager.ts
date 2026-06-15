a3e7d61e16caa33024cdcd6ad5afb0b84ceb8ff3
edgar.treischl <edgar.treischl@isb.bayern.de>
2026-06-15 07:13:59 +0200

    added theme

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,13 +1,5 @@
 import { addons } from '@storybook/manager-api'
-import { create } from '@storybook/theming'
-
-const theme = create({
-  base: 'light',
-  brandTitle:
-    '<img src="./dash-logo.svg" alt="Dash UI Components" style="height:64px;width:auto;display:block;" />',
-  brandImage: null,
-  brandUrl: 'https://edgar-treischl.github.io/dash-storybook/?path=/',
-})
+import theme from './DashTheme'
 
 addons.setConfig({
   theme,
